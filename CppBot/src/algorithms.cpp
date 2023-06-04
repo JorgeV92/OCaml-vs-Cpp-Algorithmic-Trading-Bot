7cd1480620765a094c0f39d5aef6f426910c1888
shihyunnam <73150912+shihyunnam@users.noreply.github.com>
2023-06-03 23:04:24 -0500

    general performing moving average function working

--- CppBot/src/algorithms.cpp
+++ CppBot/src/algorithms.cpp
@@ -1,13 +1,17 @@
 #include<algorithms.h>
 // Function to calculate the moving average of prices
 //strategy 1
-double algo::calculateMovingAverage(const std::vector<double>& prices) {
-    double sum = 0.0;
-    // for (const auto& price : prices) {
-    //     sum += price;
-    // }
-    for(size_t i = 0; i < prices.size(); i++) {
-        sum += prices[i];
+std::vector<double> algo::calculateMovingAverage(const std::vector<double>& prices, int windowSize) {
+    std::vector<double> movingAverages;
+
+    for (int i = 0; i <= prices.size() - windowSize; i++) {
+        double sum = 0.0;
+        for (int j = i; j < i + windowSize; j++) {
+            sum += prices[j];
+        }
+        double average = sum / windowSize;
+        movingAverages.push_back(average);
     }
-    return (sum / prices.size());
+
+    return movingAverages;
 }
